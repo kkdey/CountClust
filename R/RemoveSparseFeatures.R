d22b21659856e805cc140879b835719a3fef3827
Kushal Kumar Dey <kkdey@uchicago.edu>
2015-09-07 16:31:37 -0500

    mods on RemoveSparseFeatures

--- R/RemoveSparseFeatures.R
+++ R/RemoveSparseFeatures.R
@@ -3,20 +3,20 @@
 #' @param  data counts data
 #' @param  filter_prop   threshold proportion, if the proportion of zeros for the feature exceeds this threshold
 #'                       then we remove the feaature altogether (default is 0.9)
-#' 
+#'
 #' @author Kushal K Dey
-#' 
-#' @description This function deals with zero counts in the counts dataset. If for a feature, the proportion of 
+#'
+#' @description This function deals with zero counts in the counts dataset. If for a feature, the proportion of
 #' zeros across the samples is greater than filter_prop, then we remove the feature.
-#' 
+#'
 #' @keywords counts data, feature extraction
-#' 
+#'
 #' @export
-#' @examples 
+#' @examples
 #' mat <- rbind(c(2,0,3,0,4),c(4,5,5,0,0),c(30,34,63,25,0),c(0,0,0,0,0));
 #' RemoveSparseFeatures(mat,filter_prop=0.5)
 #' RemoveSparseFeatures(mat)
-#'   
+#'
 
 
 
@@ -28,7 +28,7 @@ RemoveSparseFeatures <- function(data, filter_prop=0.9)
     ll <- list("data"=data,"sparse_features"=as.numeric())
     return(ll)
   } else{
-    features_to_remove <- which(zero_prop_cols > filter_prop)
+    features_to_remove <- colnames(data)[which(zero_prop_cols > filter_prop)]
     data <- as.matrix(data[,-features_to_remove]);
     ll <- list("data"=data, "sparse_features"=features_to_remove)
     return(ll)
