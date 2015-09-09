be91af55c9a63a3d2ac6054c6992e71f0debe520
Kushal K Dey <kshldey@gmail.com>
2015-09-08 19:31:04 -0500

    added control parameters for Structure plot, both to StructureObj and countsclust functions

--- R/structureObj.R
+++ R/structureObj.R
@@ -9,6 +9,8 @@
 #' @param path The directory path where we want to save the data and Structure plots.
 #' @param partition A logical vector of same length as metadata. partition[i]=TRUE will imply that for the Structure
 #'            plot for i th metadata, no vertical line parititon between classes is used.
+#' @param control() A list of control parameters for the Structure plot. The control list has the arguments
+#'        struct.width, struct.height, cex.axis, cex.main, las.struct and las.labels.
 #'
 #' @description This function takes the counts data (no. of samples x no. of features) and the value of K, the number of topics or
 #' cluster to fit, along with sample metadata information and fits the topic model (due to Matt Taddy, check package
@@ -28,8 +30,24 @@
 
 
 
-StructureObj <- function(data, nclus, samp_metadata, tol, batch_lab, path, partition=rep('TRUE',ncol(samp_metadata)))
+StructureObj <- function(data, nclus, samp_metadata, tol, batch_lab, path,
+                         partition=rep('TRUE',ncol(samp_metadata)),
+                         control=list())
 {
+  control.default <- list(struct.width=800, struct.height=250, cex.axis=0.5, cex.main=1.5, las.struct=1, las.labels=2);
+
+   namc=names(control)
+  if (!all(namc %in% names(control.default)))
+    stop("unknown names in control: ", namc[!(namc %in% names(control.default))])
+  control=modifyList(control.default, control)
+
+  struct.width <- control$struct.width;
+  struct.height <- control$struct.height;
+  cex.axis <- control$cex.axis;
+  cex.min <- control$cex.main;
+  las.struct <- control$las.struct;
+  las.labels <- control$las.labels;
+
   message('Fitting the topic model (due to Matt Taddy)', domain = NULL, appendLF = TRUE)
   Topic_clus <- topics(data, K=nclus, tol=tol);
   docweights <- Topic_clus$omega;
@@ -44,18 +62,18 @@ StructureObj <- function(data, nclus, samp_metadata, tol, batch_lab, path, parti
     metadata_vec <- samp_metadata[,num];
     metadata_ordered <- metadata_vec[order(metadata_vec)];
     docweights_ordered <- docweights[order(metadata_vec),];
-    png(filename=paste0(path,'/struct_clus_',nclus,'_',colnames(samp_metadata)[num],'.png'),width=500, height=300);
+    png(filename=paste0(path,'/struct_clus_',nclus,'_',colnames(samp_metadata)[num],'.png'),width=struct.width, height=struct.height);
     barplot(t(docweights_ordered),col=2:(nclus+1),axisnames=F,space=0,border=NA,
             main=paste("Structure arranged by",colnames(samp_metadata)[num],": topics=",nclus),
-            las=1,ylim=c(0,1),ylab="admix prop", xlab=paste0(colnames(samp_metadata)[num]),
-            cex.axis=0.3,cex.main=1.4);
+            las=las.struct,ylim=c(0,1),ylab="admix prop", xlab=paste0(colnames(samp_metadata)[num]),
+            cex.axis=cex.axis,cex.main=cex.main);
     labels = match(unique(metadata_ordered), metadata_ordered);
     if(partition[num]=='TRUE') abline(v=labels-1)
 
     labels_low=labels-1;
     labels_up=c(labels_low[2:length(labels_low)],dim(docweights_ordered)[1]);
     mid_point <- labels_low +0.5*(labels_up-labels_low);
-    axis(1,at=mid_point, unique(metadata_ordered),las=2,cex.axis=0.3);
+    axis(1,at=mid_point, unique(metadata_ordered),las=las.labels,cex.axis=cex.axis);
     dev.off()
 
   }
@@ -64,18 +82,18 @@ StructureObj <- function(data, nclus, samp_metadata, tol, batch_lab, path, parti
     batch_vec <- batch_lab;
     batch_vec_ordered <- batch_vec[order(batch_vec)];
     docweights_ordered <- docweights[order(batch_vec),];
-    png(filename=paste0(path,'/struct_clus_',nclus,'_batch.png'),width=500, height=300);
+    png(filename=paste0(path,'/struct_clus_',nclus,'_batch.png'),width=struct.width, height=struct.height);
     barplot(t(docweights_ordered),col=2:(nclus+1),axisnames=F,space=0,border=NA,
             main=paste("Structure arranged by batch",": topics=",nclus),
-            las=1,ylim=c(0,1),ylab="admix prop", xlab="batch",
-            cex.axis=0.3,cex.main=1.4);
+            las=las.struct,ylim=c(0,1),ylab="admix prop", xlab="batch",
+            cex.axis=cex.axis,cex.main=cex.main);
     labels = match(unique(batch_vec_ordered), batch_vec_ordered);
     if(partition[num]=='TRUE')  abline(v=labels-1)
 
     labels_low=labels-1;
     labels_up=c(labels_low[2:length(labels_low)],dim(docweights_ordered)[1]);
     mid_point <- labels_low +0.5*(labels_up-labels_low);
-    axis(1,at=mid_point, unique(batch_vec_ordered),las=2,cex.axis=0.3);
+    axis(1,at=mid_point, unique(batch_vec_ordered),las=las.labels,cex.axis=cex.axis);
     dev.off()
   }
 
