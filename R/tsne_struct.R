60f7e28fe1248c43c562d0072c7a4aec4fe5c1c3
Kushal K Dey <kshldey@gmail.com>
2015-09-17 13:38:21 -0500

    exported tsne_struct

--- R/tsne_struct.R
+++ R/tsne_struct.R
@@ -3,30 +3,34 @@
 #' @param  omega the topic proportion matrix obtained from StructureObj/ topics ()  in maptpx
 #' @param  samp_metadata The sample labels used for identifying labels in tSNE plot
 #' @param  path  The path where you want to save the t-SNE plot
-#' 
+#'
 #' @description This function transforms the simplex matrix omega to unconstrained matrix and then applies t-SNE (Van der Maaten) and plots the results on 2D
-#' 
+#'
 #' @references L.J.P. van der Maaten. Accelerating t-SNE using Tree-Based Algorithms. Journal of Machine Learning Research, 2014:3221-3245
-#' 
+#'
 #'            L.J.P. van der Maaten and G.E. Hinton. Visualizing High-Dimensional Data Using t-SNE. Journal of Machine Learning Research, 2008: 2579-2605.
 #'
 #'            Broman KW. R/qtlcharts: interactive graphics for quantitative trait locus mapping. 2015. Genetics 199:359-361
 
+#' @author Kushal K Dey
+#' @keywords tsne, topic model
+#' @export
+
 
 tsne_struct <- function(omega,samp_metadata,path)
 {
-  reverse_transform=function(x) 
+  reverse_transform=function(x)
   {
     return(log((abs(x[2:length(x)])+1e-7)/(abs(x[1])+1e-7)));
   }
-  
-  transform <- function(y) 
+
+  transform <- function(y)
   {
     temp =c(1,exp(y));
     out=temp/sum(temp);
     return(out)
   }
-  
+
   rev_omega <- apply(omega,1,function(x) reverse_transform(x));
   tsne_out <- tsne(rev_omega,2);
   if(is.null(samp_metadata)){
