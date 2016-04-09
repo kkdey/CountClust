c4703d632b231539394f20701734e8082601e54c
jhsiao999 <joyce.hsiao1@gmail.com>
2016-04-09 17:48:33 -0500

    edit roxygen comments

--- R/ExtractTopFeatures.R
+++ R/ExtractTopFeatures.R
@@ -4,14 +4,15 @@
 #'        clusters and applies a KL-divergence based method to
 #'        obtain a list of top features that drive each of the clusters.
 #'
-#' @param theta \eqn{\theta} matrix the relative gene expression profile of the GoM clusters
+#' @param theta \eqn{\boldsymbol{theta}} matrix, the relative gene expression profile of the GoM clusters
 #'                (cluster probability distributions) 
-#'                from the GoM model fitting (a \eqn{G x K} matrix where G is
+#'                from the GoM model fitting (a \eqn{G x K} matrix where \eqn{G} is
 #'                number of features, \eqn{K} number of topics).
-#' @param top_features  The number of top features per cluster that drives
-#'                        away that cluster from others. Default value is 10.
+#' @param top_features  The top features in each cluster \eqn{k} that are selected based on the feature's
+#'                          ability to distinguish cluster \eqn{k} from cluster \eqn{1, \dots, K} 
+#'                          for all cluster \eqn{k \ne l}. Default: \eqn{10}.
 #' @param method  The underlying model assumed for KL divergence measurement.
-#'                  Two choices considered are "bernoulli" and "poisson".
+#'                  Two choices considered are "bernoulli" and "poisson". Default: poisson.
 #' @param options if "min", for each cluster k, we select features that
 #'                  maximize the minimum KL divergence of cluster k against
 #'                  all other clusters for each feature. If "max", we select
@@ -35,7 +36,7 @@ ExtractTopFeatures <- function(theta,
                                method = c("poisson","bernoulli"),
                                options=c("min", "max"))
 {
-    if (!is.null(method)) {
+    if (is.null(method)) {
         warning("method is not specified! Default method is Poisson distribution.")        
         method <- "poisson"
     }
